1eeb9a78d08c632330c2a75a503a6d99b6c83681
AAllahverdi2 <agamaliyevallahverdi2@gmail.com>
2024-11-07 21:54:16 +0400

    added skill

--- schema/Protfolio.schema.js
+++ schema/Protfolio.schema.js
@@ -2,7 +2,6 @@ const mongoose=require("mongoose")
 const Portfolio=mongoose.model("portfolio",new mongoose.Schema({
     name:String,
     desc:String,
-    price:Number,
     image:String
 }) )
 module.exports={Portfolio}
